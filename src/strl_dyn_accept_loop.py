64e2b38657c3c2ce7d807fc5da11fa0a147a0954
lukasvoege <lukas.voege@web.de>
2022-01-06 23:04:14 +0100

    minor changes

--- src/strl_dyn_accept_loop.py
+++ src/strl_dyn_accept_loop.py
@@ -48,6 +48,7 @@ if start_btn:
     plot_element = st.empty()
     progress_element = st.empty()
     text_element = st.empty()
+    iter_info = 'Simulation Start'
 
     sim_iteration = sim.run()
 
@@ -62,10 +63,15 @@ if start_btn:
         ax.plot(x, metrics["oracle"]["holdout"]['roc_auc'], label = 'roc_auc-oracle')
         ax.legend(loc = 'lower right')
         plt.ylim([0.5, 1.0])
+        plt.ylabel('roc_auc')
+        plt.xlabel('# Iteration')
         plot_element.pyplot(fig)
         plt.close()
 
         progress_element.progress((iteration[0]/(n_years - 1)))
-        text_element.write(f'Itteration: {iteration[0]}) Accepted: {iteration[1].count(True)} | Denied: {iteration[1].count(False)} - New train set size: {iteration[2]}')
-    
-    st.balloons()
+        iter_info += '\n' + f'Iteration: {iteration[0]}) Accepted: {iteration[1].count(True)} | Denied: {iteration[1].count(False)} - New train set size: {iteration[2]}'
+        text_element.text_area('Iteration Summary', iter_info)
+
+    iter_info += '\n' + 'Simulation End'
+    text_element.text_area('Iteration Summary', iter_info)
+    #st.balloons()
