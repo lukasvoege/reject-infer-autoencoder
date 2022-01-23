e5e464e42c854a571f81d766354480f5310a42bf
Jan Papmeier <jan@papmeier.de>
2022-01-23 15:08:02 +0100

    fixed error in EMsemisupervised

--- src/reject_inference.py
+++ src/reject_inference.py
@@ -9,8 +9,8 @@ def augmentation(model, x,y, iterative: bool = False):
     model.fit(x,y,weights)
 
 
-def  EMsemisupervised(model, X,y):
-    params = learn_params(X,y)
+def  EMsemisupervised(model, X,y, accepted):
+    params = learn_params(X,accepted)
     
     weights = [1 - params["phi"], params["phi"]]
     means = [params["mu0"], params["mu1"]]
@@ -21,17 +21,17 @@ def  EMsemisupervised(model, X,y):
                             tol=0.01,
                             max_iter=1000,
                             weights_init=weights,
-                            means_init=means)#,
-                            #precisions_init=covariances)
+                            means_init=means,
+                            precisions_init=covariances)
     mixture.fit(X)
     predicts = mixture.predict_proba(X)
     model.fit(X,y,predicts[:,0])
 
 def learn_params(x_labeled, y_labeled):
     n = x_labeled.shape[0]
-    phi = x_labeled[y_labeled == 1].shape[0] / n
-    mu0 = np.sum(x_labeled[y_labeled == 0], axis=0) / x_labeled[y_labeled == 0].shape[0]
-    mu1 = np.sum(x_labeled[y_labeled == 1], axis=0) / x_labeled[y_labeled == 1].shape[0]
-    sigma0 = np.cov(x_labeled[y_labeled == 0].T, bias= True)
-    sigma1 = np.cov(x_labeled[y_labeled == 1].T, bias=True)
+    phi = x_labeled[y_labeled].shape[0] / n
+    mu0 = np.sum(x_labeled[y_labeled], axis=0) / x_labeled[y_labeled == 0].shape[0]
+    mu1 = np.sum(x_labeled[y_labeled], axis=0) / x_labeled[y_labeled == 1].shape[0]
+    sigma0 = np.cov(x_labeled[y_labeled].T, bias= True)
+    sigma1 = np.cov(x_labeled[y_labeled].T, bias=True)
     return {'phi': phi, 'mu0': mu0, 'mu1': mu1, 'sigma0': sigma0, 'sigma1': sigma1}
