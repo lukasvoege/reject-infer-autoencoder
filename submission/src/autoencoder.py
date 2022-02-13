90032ffa74f05c03b5f9fa1ccd8bc2abd6ffab41
lukasvoege <lukas.voege@web.de>
2022-02-13 18:59:33 +0100

    created submission folder to zip

---
+++ submission/src/autoencoder.py
@@ -0,0 +1,165 @@
+from tkinter import E
+from turtle import shape
+import pandas as pd
+import numpy as np
+import torch
+import torch.nn as nn
+import torch.optim as optim
+import torch.distributions as dis
+from sklearn.preprocessing import StandardScaler
+from torch.utils.data import Dataset, DataLoader
+import mmd_loss as mmd
+
+import helper as h
+
+
+# Custom Dataset class, that loads the standardized and prepared data from function load_data_to_tensor()
+class CreditscoringDataset(Dataset):
+    def __init__(self, dataset_name):
+        self.x, self.y = load_data_to_tensor(dataset_name)
+        
+    def __getitem__(self,index):      
+        return self.x[index], self.y[index]
+
+    def __len__(self):
+        return self.x.shape[0]
+
+
+# Autoencoder
+class Autoencoder(nn.Module):
+    def __init__(self, shape):
+        super(Autoencoder, self).__init__()
+
+        if shape[0] != shape[-1]:
+            print('Warning! First and last layer of encoder do not have the same size.')
+
+        self.enc = nn.ModuleList()
+        self.dec = nn.ModuleList()
+
+        # Build encoder part
+        for i in range(shape.index(min(shape))):
+            self.enc.append(nn.Linear(in_features = shape[i], out_features = shape[i + 1]))
+        
+        # Build decoder part
+        for i in range(shape.index(min(shape)), len(shape) - 1):
+            self.dec.append(nn.Linear(in_features = shape[i], out_features = shape[i + 1]))
+
+    def forward(self, x):
+        x = self.decode(self.encode(x))
+        return x
+
+    def encode(self, x):
+        for e in self.enc:
+            x = torch.tanh(e(x))
+        return x
+        
+    def decode(self, x):
+        for d in self.dec:
+            x = torch.tanh(d(x))
+        return x
+
+
+# train any net
+def train(net, trainloader, epochs, learningrate, lossFuncWeights, verbose = True):
+    criterion = nn.MSELoss()
+    criterion2 = nn.KLDivLoss(log_target=True, reduction="batchmean")
+    criterion3 = mmd.MMD_loss()
+    optimizer = optim.Adam(net.parameters(), lr=learningrate)
+
+    train_loss = []
+    train_loss_mmse = []
+    train_loss_mmd = []
+    train_loss_kld = []
+    for epoch in range(epochs):
+        running_loss = 0.0
+        running_loss_mmse = 0.0
+        running_loss_mmd = 0.0
+        running_loss_kld = 0.0
+        for data in trainloader:
+            data_x, data_y = data
+            optimizer.zero_grad()
+            outputs = net(data_x)
+            encoded = net.encode(data_x)
+
+            # split encoded data into good and bad subsets
+            good = [True if x == 0 else False for x in data_y]
+            enc_good = encoded[good]
+            enc_bad = encoded[[not value for value in good]]
+            #print(f'Enc_good shape: {enc_good.shape} Enc_bad shape: {enc_bad.shape}')
+
+            # build MultiNorm Distributions from subsets and create log_probs of enc_good for both distributions to compare with KLDIVLOSS
+            MN_dist_good = dis.multivariate_normal.MultivariateNormal(torch.mean(enc_good, dim=0), torch.corrcoef(torch.transpose(enc_good, 0, 1)))
+            MN_dist_bad = dis.multivariate_normal.MultivariateNormal(torch.mean(enc_bad, dim=0), torch.corrcoef(torch.transpose(enc_bad, 0, 1)))
+
+            sample = MN_dist_good.sample((1000,))
+
+            enc_good = enc_good[:min([len(enc_good),len(enc_bad)])]
+            enc_bad = enc_bad[:min([len(enc_good),len(enc_bad)])]
+
+            # calculate criterions only if they influence the overall loss
+            MMSELoss = criterion(outputs, data_x)                                                            if lossFuncWeights[0] > 0.0 else torch.zeros(1)
+            KLDivLoss = criterion2(MN_dist_bad.log_prob(sample), MN_dist_good.log_prob(sample))  * 1000000   if lossFuncWeights[1] > 0.0 else torch.zeros(1)
+            MMDLoss = criterion3(enc_good,enc_bad) * 10                                                      if lossFuncWeights[2] > 0.0 else torch.zeros(1)
+
+            sum_loss = MMSELoss.item() + KLDivLoss.item() + MMDLoss.item()
+            MMSELoss = sum_loss * MMSELoss / MMSELoss.item() 
+            KLDivLoss = sum_loss * KLDivLoss / KLDivLoss.item()
+            MMDLoss = sum_loss * MMDLoss / MMDLoss.item()
+
+            loss = lossFuncWeights[0] * MMSELoss + lossFuncWeights[1] * KLDivLoss + lossFuncWeights[2] * MMDLoss
+
+            loss.backward()
+            optimizer.step()
+            running_loss += loss.item()
+            running_loss_mmse += lossFuncWeights[0] * MMSELoss.item()
+            running_loss_mmd += lossFuncWeights[2] * MMDLoss.item()
+            running_loss_kld += lossFuncWeights[1] * KLDivLoss.item()
+
+        loss = running_loss / len(trainloader)
+        MMSELoss = running_loss_mmse / len(trainloader)
+        MMDLoss = running_loss_mmd / len(trainloader)
+        KLDivLoss = running_loss_kld / len(trainloader)
+        train_loss.append(loss)
+        train_loss_mmse.append(MMSELoss)
+        train_loss_mmd.append(MMDLoss)
+        train_loss_kld.append(KLDivLoss)
+        
+        if verbose: print('Epoch {} of {}, Train Loss: {:.4f} (MMSE: {:.4f} | MMD: {:.4f} | KLD: {:.4f})'.format(epoch+1, epochs, loss, MMSELoss, MMDLoss, KLDivLoss))
+
+    return train_loss, train_loss_mmse, train_loss_mmd, train_loss_kld
+
+
+# load data from csv file, woe encode categorical features (TO-DO), standardize values, make tensor with shape [n_rows, n_features]
+def load_data_to_tensor(dataset_name):
+    complete_data = pd.read_csv(f'../prepared_data/{dataset_name}', sep=',')
+    complete_data['BAD'] = np.where(complete_data['BAD'] == 'BAD', 1, 0).astype(np.int64)
+
+    # We down sample the GOOD datapoints to even out the imbalance in the data set
+    complete_data = pd.concat([complete_data[complete_data['BAD'] == 0].sample(complete_data['BAD'].value_counts()[1],random_state=37841), complete_data[complete_data['BAD'] == 1]])
+
+    obj_cols = complete_data.select_dtypes('object').columns
+    complete_data[obj_cols] = complete_data[obj_cols].astype('category')
+
+    # For the sake of simplicity when dealing with neural nets later, let's just make everything categorical continous
+    for col in obj_cols:
+        woe_calc = h.IV_Calc(complete_data, feature=col, target='BAD')
+        woe = woe_calc.full_summary()['WOE_adj'].to_dict()
+        complete_data[col] = complete_data[col].map(woe)
+        complete_data[col] = complete_data[col].astype('float64')
+
+    ###### TRAIN ENCODER ON SUBSET OF THE DATA ########################
+    #complete_data = complete_data[complete_data['BAD'] == 0]    # Only on BAD (1) or GOOD (0)
+    #print(f'Shape of Autoencoder training data: {complete_data.shape}')
+    ###################################################################
+
+    complete_X = complete_data.iloc[:, complete_data.columns != 'BAD']
+    complete_y = complete_data['BAD']
+
+    x_np = complete_X.values.reshape(-1, complete_X.shape[1]).astype('float32')
+    y_np = complete_y.values.reshape(-1, 1).astype('float32')
+
+    # stadardize values
+    standardizer = StandardScaler()
+    x_stand = standardizer.fit_transform(x_np)
+
+    return torch.from_numpy(x_stand), torch.from_numpy(y_np)
---
+++ submission/src/gridsearch.py
@@ -0,0 +1,122 @@
+import sys
+sys.path.append('../src')
+
+import importlib
+import dyn_accept_loop as dal
+importlib.reload(dal)
+import reject_inference as rinf
+importlib.reload(rinf)
+import helper as h
+importlib.reload(h)
+import lightgbm as lgbm
+import numpy as np
+import pickle
+
+import autoencoder as aenc
+import var_autoencoder as vaenc
+import importlib
+importlib.reload(aenc)
+importlib.reload(vaenc)
+
+import timeit
+
+
+
+n_years = 20
+model = lgbm.LGBMClassifier(random_state=1234) # DecisionTreeClassifier(min_samples_leaf=40) LogisticRegression(max_iter=400)
+datasetname = "gmsc.csv"
+
+## ------------------------------------------------------------
+## Get a Baseline Bias Measue without any reject inference
+## ------------------------------------------------------------
+
+sim = dal.Simulate_acceptance_loop(datasetname, model, 0.1, 0.1, n_years, enc_features=False, rej_inf=None)
+results_generator = sim.run()
+
+metrics = None
+for iteration in results_generator:
+    #print(f'Itteration: {iteration[0]}) Accepted: {iteration[1].count(True)} | Denied: {iteration[1].count(False)} - New train set size: {iteration[2]}')
+    metrics = iteration[3]
+
+last_n_years = 5
+baseline_bias = h.measure_bias(metrics["oracle"]["holdout"]['roc_auc'], metrics["model"]["holdout"]['roc_auc'], last_n_years)
+baseline_roc_auc = sum(metrics["model"]["holdout"]['roc_auc'][-last_n_years:]) / last_n_years
+print(f'Baseline Sampling Bias: {round(baseline_bias, 5)}\nBaseline ROC-AUC: {round(baseline_roc_auc, 5)}')
+
+
+## -----------------------------------------------------------------------
+## Loop through Autoencoder training and testing for a parameter
+## -----------------------------------------------------------------------
+
+weight_test = np.array(range(0, 11, 1)) / 10 
+epochs_test = [1] + list(range(1, 7, 1))    
+shape_test = np.array(range(4, 40, 3))      
+BATCH_SIZE = 2000
+LR = 1e-3
+
+sampling_bias = dict()
+sampling_bias_flat = []
+roc_auc = dict()
+roc_auc_flat = []
+
+
+for weight in weight_test:
+    sampling_bias[weight] = dict()
+    roc_auc[weight] = dict()
+    for layer in shape_test:
+
+        start = timeit.default_timer()
+        
+        # Train Autoencoder
+        LOSSFUNCWEIGHTS = [weight, 1 - weight, 0.0]  #[MMSE, KLDiv, MMD]
+
+        dataset = aenc.CreditscoringDataset(datasetname)      # load and prepare Dataset to Tensor
+        data_loader = aenc.DataLoader(                       # create Dataloader for batching
+            dataset, 
+            batch_size=BATCH_SIZE,
+            shuffle=True,
+            drop_last=True
+        )
+
+        shape = [dataset.x.shape[1], 45, layer, 45, dataset.x.shape[1]]  
+        net3 = aenc.Autoencoder(shape)
+        net3.to("cpu")
+
+        sampling_bias[weight][layer] = []
+        roc_auc[weight][layer] = []
+
+        prev_ep = 0
+        for EPOCHS in epochs_test:
+
+            train_loss, train_loss_mmse, train_loss_mmd, train_loss_kld = aenc.train(net3, data_loader, 2**EPOCHS, LR, LOSSFUNCWEIGHTS, verbose = False)
+
+            # Simulate on encoded Data to measure sampling bias
+
+            sim = dal.Simulate_acceptance_loop(datasetname, model, 0.1, 0.1, n_years, enc_features=True, encoder=net3)
+            results_generator = sim.run()
+
+            metrics7 = None
+            for iteration in results_generator:
+                metrics7 = iteration[3]
+
+
+            sampling_bias[weight][layer].append(h.measure_bias(metrics["oracle"]["holdout"]['roc_auc'], metrics7["model"]["holdout"]['roc_auc'], last_n_years))
+            roc_auc[weight][layer].append(sum(metrics7["model"]["holdout"]['roc_auc'][-last_n_years:]) / last_n_years)
+
+            sampling_bias_flat.append(h.measure_bias(metrics["oracle"]["holdout"]['roc_auc'], metrics7["model"]["holdout"]['roc_auc'], last_n_years))
+            roc_auc_flat.append(sum(metrics7["model"]["holdout"]['roc_auc'][-last_n_years:]) / last_n_years)
+
+            print(f'PARAM: W({weight}), L({layer}), E({prev_ep + 2**EPOCHS}) | Sampling Bias: {round(sampling_bias[weight][layer][-1], 5)} // ROC-AUC: {round(roc_auc[weight][layer][-1], 5)}')
+
+            prev_ep += 2**EPOCHS
+        
+        stop = timeit.default_timer()
+
+        print(f'Saving current results... - Last iteration took {stop - start} sec.')
+        pickle.dump(roc_auc, open('roc-auc_grid-result.p', 'wb'))
+        pickle.dump(roc_auc_flat, open('roc-auc-flat_grid-result.p', 'wb'))
+
+        pickle.dump(sampling_bias, open('sampling-bias-result.p', 'wb'))
+        pickle.dump(sampling_bias_flat, open('sampling-bias-flat-result.p', 'wb'))
+
+print("I am done!")
